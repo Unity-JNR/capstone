9a645e88aa025e5b0db352d76bede1adc16a8b57
Unity-JNR <unitydemas12@gmail.com>
2024-03-07 16:18:07 +0200

    changed something

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(cors())
+app.use(cors({
+  origin: 'http://localhost:8080',
+  credentials: true
+}))
 app.use(express.json());
 app.use(express.static('public'))
 
