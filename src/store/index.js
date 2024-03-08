db9c70d300185812e1bb20dc3cbdbbf43a46919a
Unity-JNR <unitydemas12@gmail.com>
2024-03-08 10:29:05 +0200

    added login

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,28 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
+axios.defaults.withCredentials = true
+
+const loginn = 'http://localhost:8086/login'
 
 export default createStore({
   state: {
+    loginIn: false
   },
   getters: {
   },
   mutations: {
+    setLogged(state,value){
+      state.loginIn = value
+    }
   },
   actions: {
+   async login({commit},user){
+      let {data} = await axios.post(loginn,user)
+      console.log(data);
+      $cookies.set('jwt',data.token)
+      alert(data.msg)
+      commit('setLogged',true)
+    }
   },
   modules: {
   }
