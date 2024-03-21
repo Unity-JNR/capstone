f07d67310de87345420027912b1bac06ade7a437
Unity-JNR <unitydemas12@gmail.com>
2024-03-21 19:39:30 +0200

    fixed error

--- middleware/errorHandling.js
+++ middleware/errorHandling.js
@@ -1,14 +1,11 @@
 function errorHandling(err, req, res, next) {
-    if (err || (res && res.statusCode >= 400)) {
+    if (err || res.statusCode >= 400) {
         res.status(err.status || res.statusCode || 500).json({
             statusCode: err.status || res.statusCode || 500,
             msg: 'Apologies, there seems to be an error in the server. Please try again later.'
         });
-    } else if (next) {
-        next();
     } else {
-        // If `next` is not defined, end the request-response cycle
-        res.end();
+        next(); // Pass control to the next middleware
     }
 }
 
