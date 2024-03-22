cc7d1901d520e0526d5b1aa4649a05b5729c57f1
Unity-JNR <unitydemas12@gmail.com>
2024-03-22 09:48:14 +0200

    added changes

--- controller/cart.js
+++ controller/cart.js
@@ -26,7 +26,7 @@ export default {
     res.send(await getcarts())
    },
    deleteall: async (req,res)=> {
-      await  deletecarts(+req.params.id);
+      await  deletecarts(user,+req.params.id);
       res.send(await getcarts())
    }
 
