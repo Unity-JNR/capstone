644d9cedc4f5f3c665d1a047030f3bc7f09292f2
Unity-JNR <unitydemas12@gmail.com>
2024-09-05 14:48:00 +0200

    removed something

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,53 +1,69 @@
-import {config} from 'dotenv'
-import bcrypt from 'bcrypt'
-// import cookieParser from 'cookie-parser'
-import jwt from 'jsonwebtoken'
-import { checkuser,getusername } from '../models/database.js'
+import { config } from 'dotenv';
+import bcrypt from 'bcrypt';
+// import cookieParser from 'cookie-parser'; // Unused import
+import jwt from 'jsonwebtoken';
+import { checkuser, getusername } from '../models/database.js';
 
-config()
+config();
 
-const authenticate=(req,res,next)=>{
-    let {cookie}= req.headers
-    let tokenInHeader=cookie && cookie.split('=')[1]
+const authenticate = (req, res, next) => {
+  const { cookie } = req.headers; // Destructuring cookie from headers
 
-    if(tokenInHeader===null) res.sendStatus(401)
+  if (!cookie) {
+    // Check if the cookie header is missing
+    return res.sendStatus(401); // Unauthorized if no cookie is present
+  }
 
-    jwt.verify(tokenInHeader,process.env.SECRET_KEY,(err,user)=>{
- 
+  // Extract token from cookie; assumes cookie format is 'jwt=<token>'
+  const tokenInHeader = cookie
+    .split('; ')
+    .find((row) => row.startsWith('jwt='))
+    ?.split('=')[1];
 
-        if(err) return res.sendStatus(403)
-        req.user=user
-        next()
-    })
-}
- 
+  if (!tokenInHeader) {
+    // Check if the token is missing
+    return res.sendStatus(401); // Unauthorized if no token is present
+  }
 
-const auth=async(req,res,next)=>{
-    const {userPass,userName}=req.body
-    const hashedPassword=await checkuser(userName)
-    let thisUser = await getusername(userName)
-    bcrypt.compare(userPass,hashedPassword,(err,result)=>{
+  jwt.verify(tokenInHeader, process.env.SECRET_KEY, (err, user) => {
+    if (err) {
+      return res.sendStatus(403); // Forbidden if the token is invalid
+    }
+    req.user = user; // Attach the user object to the request
+    next(); // Proceed to the next middleware or route handler
+  });
+};
 
-        if(err) throw err
-        if(result === true){
-            const {userName}=req.body
+const auth = async (req, res, next) => {
+  const { userPass, userName } = req.body;
+  const hashedPassword = await checkuser(userName);
+  const thisUser = await getusername(userName);
 
-            const token=jwt.sign({userName:userName},process.env.SECRET_KEY,{expiresIn:'1h'})
-            res.cookie('jwt',token,thisUser,{httpOnly:true,secure:true,sameSite:'none',maxAge:3600000})
+  bcrypt.compare(userPass, hashedPassword, (err, result) => {
+    if (err) throw err;
+    if (result === true) {
+      const token = jwt.sign({ userName: userName }, process.env.SECRET_KEY, { expiresIn: '1h' });
+      
+      // Set the JWT token in a cookie with proper options
+      res.cookie('jwt', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none',
+        maxAge: 3600000, // 1 hour in milliseconds
+      });
 
-            res.send({
-                token:token,
-                msg:'i have logged in!!! YAY!!!',
-                user:thisUser
-            })
-            next()
-        }else{
-            res.send({msg:'The userName or password is incorrect'})
-        }
-    })
-}
+      res.send({
+        token: token,
+        msg: 'I have logged in!!! YAY!!!',
+        user: thisUser,
+      });
+    } else {
+      res.send({ msg: 'The username or password is incorrect' });
+    }
+  });
+};
 
-
-export{
-    auth,authenticate
-}
+export {
+  auth,
+  authenticate,
+};
